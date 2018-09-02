4599268fe90c917e62fde8bce351970e8b671c08
Aaron <aaronmassey45@gmail.com>
2018-09-02 11:21:10 -0500

    Can create an issue

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+require('./models/issue');
+const Issue = mongoose.model('issue');
+
 mongoose.Promise = global.Promise;
 mongoose.connect(
   process.env.MONGO_URI,
@@ -12,6 +15,33 @@ mongoose.connect(
 
 const app = express();
 app.use(bodyParser.json());
+
+app.post('/api/issues/:projectName', async (req, res) => {
+  const { projectName } = req.params;
+
+  const paths = [
+    'issue_title',
+    'issue_text',
+    'created_by',
+    'assigned_to',
+    'status_text',
+  ];
+
+  const body = {};
+
+  Object.keys(req.body).forEach(key => {
+    if (paths.includes(key)) {
+      body[key] = req.body[key];
+    }
+  });
+
+  body.project = projectName;
+
+  const issue = new Issue({ ...body });
+  await issue.save();
+
+  res.send(issue);
+});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
