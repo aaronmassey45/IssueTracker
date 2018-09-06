bf6cc3ae148b3d2f613549ee417b7062be147b85
Aaron <aaronmassey45@gmail.com>
2018-09-06 10:03:58 -0500

    Updated to work with Issue component

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -1,23 +1,44 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+import Issue from './Issue';
+
 export default class Project extends Component {
   state = {
     issues: [],
   };
 
-  componentDidMount = async () => {
+  componentDidMount = () => {
+    this.getProjectData();
+  };
+
+  getProjectData = async () => {
     const res = await axios.get(
       `/api/issues/${this.props.match.params.projectName}`
     );
     const issues = res.data;
-    console.log(issues);
+    this.setState({ issues });
+  };
+
+  closeIssue = async id => {
+    await axios.put(`/api/issues/${this.props.match.params.projectName}`, {
+      id,
+      open: false,
+    });
+    this.getProjectData();
+  };
+
+  renderIssues = () => {
+    return this.state.issues.map(issue => (
+      <Issue key={issue._id} {...issue} closeIssue={this.closeIssue} />
+    ));
   };
 
   render() {
     return (
-      <div>
-        <h1>Project Page</h1>
+      <div className="container">
+        <h1>All issues for: {this.props.match.params.projectName}</h1>
+        {!this.state.issues ? <h3>Loading...</h3> : this.renderIssues()}
       </div>
     );
   }
