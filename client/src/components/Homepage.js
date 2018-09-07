02a2ab14a9a9cdff1e897814c776e1734a2f3364
Aaron <aaronmassey45@gmail.com>
2018-09-07 03:29:48 -0500

    Updated homapage info

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,10 +1,30 @@
 import React from 'react';
 
+import ProjectSearchbar from './ProjectSearchbar';
+
 export default () => {
   return (
-    <div className="App">
+    <div className="container homepage">
       <h1>Issue Tracker</h1>
-      <div>Home Page</div>
+      <h3>
+        <a
+          href="https://learn.freecodecamp.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          freeCodeCamp
+        </a>{' '}
+        Information Security and Quality Assurance Project
+      </h3>
+      <div className="tech">
+        Core Technologies Used:
+        <ul>
+          <li>ReactJs</li>
+          <li>Node/Express</li>
+          <li>MongoDB/Mongoose</li>
+        </ul>
+      </div>
+      <ProjectSearchbar />
     </div>
   );
 };
