a7c2546001405d5ab95418984539c8f05690057f
Aaron <aaronmassey45@gmail.com>
2019-10-24 04:35:22 -0500

    Changed from class component to functional component with hooks

--- client/src/components/IssueForm.js
+++ client/src/components/IssueForm.js
@@ -1,80 +1,50 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-const INITIAL_STATE = {
-  issue_title: {
-    value: '',
-    placeholder: '*Title',
-    required: true,
-  },
-  issue_text: {
-    value: '',
-    placeholder: '*Text',
-    required: true,
-  },
-  created_by: {
-    value: '',
-    placeholder: '*Created by',
-    required: true,
-  },
-  assigned_to: {
-    value: '',
-    placeholder: 'Assigned to',
-    required: false,
-  },
-  status_text: {
-    value: '',
-    placeholder: 'Status text',
-    required: false,
-  },
+import formFieldsData from 'components/issue-form/form-fields';
+import IssueFormInput from 'components/issue-form/IssueFormInput';
+
+const INITIAL_VALUES = {
+  assigned_to: '',
+  created_by: '',
+  issue_text: '',
+  issue_title: '',
+  status_text: '',
 };
 
-export default class IssueForm extends Component {
-  state = { ...INITIAL_STATE };
+const IssueForm = ({ submitNewIssue }) => {
+  const [inputValues, setInputValues] = useState({ ...INITIAL_VALUES });
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
 
-    const values = {
-      issue_text: this.state.issue_text.value,
-      issue_title: this.state.issue_title.value,
-      created_by: this.state.created_by.value,
-      status_text: this.state.status_text.value,
-      assigned_to: this.state.assigned_to.value,
-    };
-
-    this.props.submitNewIssue(values);
-    this.setState({ ...INITIAL_STATE });
+    submitNewIssue(inputValues);
+    setInputValues({ ...INITIAL_VALUES });
   };
 
-  handleChange = (e, issue) => {
-    this.setState({
-      ...this.state,
-      [issue]: { ...this.state[issue], value: e.target.value },
-    });
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setInputValues({ ...inputValues, [name]: value });
   };
 
-  renderInputs = () => {
-    return Object.keys(this.state).map(issue => {
-      return (
-        <textarea
-          className={issue}
-          key={issue}
-          onChange={e => this.handleChange(e, issue)}
-          placeholder={this.state[issue].placeholder}
-          required={this.state[issue].required}
-          rows={issue === 'issue_text' ? '10' : '1'}
-          value={this.state[issue].value}
+  return (
+    <form onSubmit={handleSubmit} className="new-issue">
+      {formFieldsData.map(({ name, placeholder, rows, required }) => (
+        <IssueFormInput
+          handleChange={handleChange}
+          key={name}
+          name={name}
+          placeholder={placeholder}
+          required={required}
+          rows={rows}
+          value={inputValues[name]}
         />
-      );
-    });
-  };
+      ))}
+      <input type="submit" value="Submit Issue!" className="submitBtn" />
+    </form>
+  );
+};
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} className="new-issue">
-        {this.renderInputs()}
-        <input type="submit" value="Submit Issue!" className="submitBtn" />
-      </form>
-    );
-  }
-}
+IssueForm.propTypes = { submitNewIssue: PropTypes.func.isRequired };
+
+export default IssueForm;
