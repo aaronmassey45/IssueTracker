472fb4eec3e06ca46aff125db8f38b9ece2d8390
Aaron <aaronmassey45@gmail.com>
2019-10-26 08:39:46 -0500

    Added sidenav

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,24 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import M from 'materialize-css';
 
+import NavLinks from 'components/navbar/NavLinks';
 import './navbar.styles.scss';
 
-const Navbar = () => (
-  <nav className="nav-wrapper grey darken-4">
-    <div className="container">
-      <Link to="/" className="brand-logo">
-        Issue Tracker
-      </Link>
-      <ul className="right">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/user-stories">User Stories</Link>
-        </li>
+const Navbar = () => {
+  React.useEffect(() => {
+    const sidenav = document.querySelector('.sidenav');
+    M.Sidenav.init(sidenav);
+  }, []);
+
+  return (
+    <>
+      <nav className="nav-wrapper grey darken-4">
+        <div className="container">
+          <Link to="/" className="brand-logo">
+            Issue Tracker
+          </Link>
+          <button href="#" data-target="mobile-nav" className="sidenav-trigger">
+            <i className="material-icons">menu</i>
+          </button>
+          <ul className="right hide-on-med-and-down">
+            <NavLinks />
+          </ul>
+        </div>
+      </nav>
+
+      <ul id="mobile-nav" className="sidenav grey darken-4">
+        <NavLinks />
       </ul>
-    </div>
-  </nav>
-);
+    </>
+  );
+};
 
 export default Navbar;
