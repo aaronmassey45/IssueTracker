5a9cbc9244331bacefcc4f10ec228c02e1865598
Aaron <aaronmassey45@gmail.com>
2019-10-26 08:40:05 -0500

    Added materialize css to App

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import 'materialize-css/dist/css/materialize.min.css';
 
 import HomePage from 'components/home-page';
 import Project from 'components/Project';
