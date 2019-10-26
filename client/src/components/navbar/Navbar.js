3ed1d0a03dfc19ca1033905028429826cdc50136
Aaron <aaronmassey45@gmail.com>
2019-10-26 08:01:13 -0500

    Added brand logo and user stories links

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './navbar.styles.scss';
+
 const Navbar = () => (
-  <ul className="navbar">
-    <li>
-      <Link to="/">Home</Link>
-    </li>
-  </ul>
+  <nav className="nav-wrapper grey darken-4">
+    <div className="container">
+      <Link to="/" className="brand-logo">
+        Issue Tracker
+      </Link>
+      <ul className="right">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/user-stories">User Stories</Link>
+        </li>
+      </ul>
+    </div>
+  </nav>
 );
 
 export default Navbar;
